3eb84a9f93e798257017514ace3183b2ede9cf75
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-26 12:49:08 +0900

    이메일 입력시 글자단위로 양식 검사   - 모든 글자를 지우면 다시 입력하도록 메세지변경

--- app/src/main/java/kr/co/yhw/sharedpreference_20200526/SignUpActivity.java
+++ app/src/main/java/kr/co/yhw/sharedpreference_20200526/SignUpActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 
 import kr.co.yhw.sharedpreference_20200526.databinding.ActivitySignUpBinding;
 
@@ -21,6 +24,36 @@ public class SignUpActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+        binding.emailEdt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String  input =s.toString();
+                Log.d("변경된 내용",input);
+
+//                @ 를 포함 + 6글자 이상 => 이메일로 인정
+                if (input.contains("@") && input.length() >= 6){
+                    binding.emailCheckResultTxt.setText("사용해도 좋은 이메일입니다.");
+                }
+                else {
+                    if(input.length() == 0){
+                        binding.emailCheckResultTxt.setText("이메일을 입력해주세요.");
+                    }
+                    else {
+                        binding.emailCheckResultTxt.setText("이메일 양식으로 입력해주세요.");
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
     }
 
